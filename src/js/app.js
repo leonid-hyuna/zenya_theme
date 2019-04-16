19c71d70e786726622cd0d4ae403daf893dabe3e
leonid-hyuna <alekseenko.leonid@hyuna.bb>
2019-04-16 19:41:50 +0300

    widget added

--- src/js/app.js
+++ src/js/app.js
@@ -155,23 +155,26 @@ $("#cancel_form_3").validate({
 	}
 });
 
-$(document).ready(function(){
-  $(".lang-flag").click(function(){
-    $(".language-dropdown").toggleClass("open");
-  });
-  $("ul.lang-list li").click(function(){
-    $("ul.lang-list li").removeClass("selected");
-    $(this).addClass("selected");
-    if($(this).hasClass('lang-en')){
-      $(".language-dropdown").find(".lang-flag").addClass("lang-en").removeClass("lang-es").removeClass("lang-pt");
-      $("#lang_selected").html("<p>EN</p>")
-    }else if($(this).hasClass('lang-pt')){
-      $(".language-dropdown").find(".lang-flag").addClass("lang-pt").removeClass("lang-es").removeClass("lang-en");
-    $("#lang_selected").html("<p>PT</p>")
-    }else{
-      $(".language-dropdown").find(".lang-flag").addClass("lang-es").removeClass("lang-en").removeClass("lang-pt");
-      $("#lang_selected").html("<p>ES</p>")
-    }
-    $(".language-dropdown").removeClass("open");
-  });
-})
+//WIDGET
+var current_lang = $('.languages').data('current-lang');
+var block_lang = $('#' + current_lang).html();
+
+
+$('.language_selector').html(block_lang);	
+	
+var userLang = navigator.language;
+var trigger = $('.language_selector');
+var list = $('.languages');
+var lang = $('.lang');
+	
+trigger.click(function() {
+    trigger.toggleClass('active_selector');
+      list.slideToggle(200);
+});
+$('.languages li a span').click(function() {
+    trigger.toggleClass('active_selector');
+  	// var texts = $(this).find("a").find("span").html();
+    var texts = $(this).parent().parent().html();
+//  	trigger.html(texts);
+    list.slideToggle(200);
+});
